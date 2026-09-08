ac51c467308461647c81d8df675651bc22661c9a
Aislan Michel <aislan.michel92@gmail.com>
2026-09-08 17:34:42 -0300

    refactor: rename template variable for clarity and streamline template validation logic

--- src/parser.lua
+++ src/parser.lua
@@ -9,7 +9,7 @@ local response = {
 }
 
 local function get_template(args)
-    local template = {
+    local template_arg = {
         hasTemplate = false,
         template_name = nil,
         count = 0
@@ -17,13 +17,13 @@ local function get_template(args)
 
     for index, arg in ipairs(args) do
         if arg == "--template" then
-            template.hasTemplate = true
-            template.template_name = args[index + 1]
-            template.count = template.count + 1
+            template_arg.hasTemplate = true
+            template_arg.template_name = args[index + 1]
+            template_arg.count = template_arg.count + 1
         end
     end
 
-    return template
+    return template_arg
 end
 
 function parser.parse_args(args)
@@ -37,20 +37,17 @@ function parser.parse_args(args)
 
     local template_arg = get_template(args)
     if template_arg.count > 1 then
-        response.success = false
         table.insert(response.problems, "Please specify only one template")
-        return response
     end
 
     if template_arg.hasTemplate == false or template_arg.template_name == nil then
-        response.success = false
         table.insert(response.problems, "Please specify a template name")
-        return response
+    elseif not template.exists(template_arg.template_name) then
+        table.insert(response.problems, "Template '" .. template_arg.template_name .. "' does not exist")
     end
 
-    if not template.exists(template_arg.template_name) then
+    if #response.problems > 0 then
         response.success = false
-        table.insert(response.problems, "Template '" .. template_arg.template_name .. "' does not exist")
         return response
     end
 
